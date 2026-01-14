25e4341c96bfdd1965b90795fd94859a09797d7b
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-01-13 22:12:54 -0300

    feat(store): implement user store

---
+++ src/shared/store/user.store.ts
@@ -0,0 +1,33 @@
+import type { User } from "@shared/interfaces/user";
+import { create } from "zustand";
+
+interface SetSessionParams {
+  user: User;
+  token: string;
+  refreshToken: string;
+}
+
+interface UpdateTokensParams {
+  token: string;
+  refreshToken: string;
+}
+
+export interface UserStore {
+  user: User | null;
+  token: string | null;
+  refreshToken: string | null;
+
+  setSession: (sessionData: SetSessionParams) => void;
+  logout: () => void;
+  updateTokens: (updateTokensData: UpdateTokensParams) => void;
+}
+
+export const useUserStore = create<UserStore>((set) => ({
+  user: null,
+  token: null,
+  refreshToken: null,
+
+  logout: () => {},
+  setSession: (sessionData) => set({ ...sessionData }),
+  updateTokens: (updateTokensData) => set({ ...updateTokensData }),
+}));
