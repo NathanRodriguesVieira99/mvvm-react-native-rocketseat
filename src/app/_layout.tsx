13dc9128229cccd664fe3f864cd0c6a87a248c08
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-01-13 22:17:29 -0300

    feat(layout): create RootLayout component with navigation stack

---
+++ src/app/_layout.tsx
@@ -0,0 +1,16 @@
+import { Stack } from "expo-router";
+import { QueryClientProvider } from "@tanstack/react-query";
+import "../styles/global.css";
+import { client } from "../lib/react-query";
+
+export default function RootLayout() {
+  return (
+    <QueryClientProvider client={client}>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="login" />
+        <Stack.Screen name="register" />
+        <Stack.Screen name="(private)" />
+      </Stack>
+    </QueryClientProvider>
+  );
+}
