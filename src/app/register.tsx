3bb2b28965bb4bbe8e70fde688b19dfb4a29a354
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-01-14 13:33:57 -0300

    feat(register): integrate RegisterService with HttpClient in useRegisterModel

--- src/app/register.tsx
+++ src/app/register.tsx
@@ -1,8 +1,14 @@
+import { HttpClient } from "@api/http-client";
 import { RegisterView } from "../presentation/view-models/register/register.view";
 import { useRegisterModel } from "../presentation/view-models/register/useRegister.model";
+import { RegisterService } from "@services/auth.service";
 
 export default function Register() {
-  const methods = useRegisterModel();
+  const http = HttpClient.create();
+
+  const registerService = new RegisterService(http);
+
+  const methods = useRegisterModel({ registerService });
 
   return <RegisterView {...methods} />;
 }
