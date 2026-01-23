46620fc2a9873f6c754dc273f8ee27b82b3cb6a6
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-01-23 19:56:10 -0300

    feat: add useLoginMutation hook for handling user login

---
+++ src/shared/mutations/useLogin.mutation.ts
@@ -0,0 +1,14 @@
+import type { LoginService } from '@services/login.service';
+import type { LoginHttpParams } from '@shared/interfaces/http/login';
+import { useUserStore } from '@shared/store/user.store';
+import { useMutation } from '@tanstack/react-query';
+
+export const useLoginMutation = (loginService: LoginService) => {
+  const { setSession } = useUserStore();
+
+  return useMutation({
+    mutationFn: (body: LoginHttpParams) => loginService.exec(body),
+    onSuccess: (response) => setSession(response),
+    onError: (err) => console.error(err.message),
+  });
+};
