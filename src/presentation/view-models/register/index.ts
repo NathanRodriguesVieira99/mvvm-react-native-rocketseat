8c2a4a40465b25b938db34c740e764f2b5ccd772
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-02-07 19:08:20 -0300

    feat: add templates with KeyboardContainer and AuthFormHeader to RegisterView

---
+++ src/presentation/view-models/register/index.ts
@@ -0,0 +1,7 @@
+import { AuthFormHeader } from '../../components/auth-form-header';
+import { KeyboardContainer } from '../../templates/keyboard-container';
+
+export const Register = {
+  KeyboardContainer: KeyboardContainer,
+  Header: AuthFormHeader,
+};
