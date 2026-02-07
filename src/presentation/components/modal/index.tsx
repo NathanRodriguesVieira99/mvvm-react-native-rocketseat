591ec09b5be7d4f1e567948d34e2bfdfb1a025ba
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-02-07 19:11:35 -0300

    feat: implement Modal component

---
+++ src/presentation/components/modal/index.tsx
@@ -0,0 +1,33 @@
+import { useModalStore } from '@shared/store/modal.store';
+import {
+  Modal as ModalComponent,
+  TouchableWithoutFeedback,
+  View,
+} from 'react-native';
+
+export const Modal = () => {
+  const isOpen = useModalStore((state) => state.isOpen);
+  const config = useModalStore((state) => state.config);
+  const content = useModalStore((state) => state.content);
+  const close = useModalStore((state) => state.close);
+
+  if (!isOpen || !content) return null;
+
+  return (
+    <ModalComponent
+      visible={isOpen}
+      animationType={config.animationType}
+      transparent={config.transparent}
+      statusBarTranslucent={config.statusBarTranslucent}
+      onRequestClose={close}
+    >
+      <TouchableWithoutFeedback onPress={close}>
+        <View className="flex-1 bg-black/50 justify-center items-center px-6">
+          <TouchableWithoutFeedback onPress={() => {}}>
+            {content}
+          </TouchableWithoutFeedback>
+        </View>
+      </TouchableWithoutFeedback>
+    </ModalComponent>
+  );
+};
