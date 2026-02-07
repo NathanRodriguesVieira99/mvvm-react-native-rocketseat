40c14b88cf6957f369f9269440799d1c9c61ec86
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-02-07 19:03:35 -0300

    feat: enhance useRegisterMutation with success callback and session management

--- src/shared/mutations/useRegister.mutation.ts
+++ src/shared/mutations/useRegister.mutation.ts
@@ -1,11 +1,29 @@
 import type { RegisterService } from '@services/register.service';
 import type { RegisterHttpParams } from '@shared/interfaces/http/register';
+import { useUserStore } from '@shared/store/user.store';
 import { useMutation } from '@tanstack/react-query';
 
-export const useRegisterMutation = (registerService: RegisterService) => {
+interface useRegisterMutationParams {
+  onSuccess?: () => void;
+  registerService: RegisterService;
+}
+
+export const useRegisterMutation = ({
+  onSuccess,
+  registerService,
+}: useRegisterMutationParams) => {
+  const setSession = useUserStore((state) => state.setSession);
+
   return useMutation({
     mutationFn: (body: RegisterHttpParams) => registerService.exec(body),
-    onSuccess: (response) => console.log(response),
+    onSuccess: (response) => {
+      setSession({
+        refreshToken: response.refreshToken,
+        token: response.token,
+        user: response.user,
+      });
+      onSuccess?.();
+    },
     onError: (err) => console.error(err),
   });
 };
