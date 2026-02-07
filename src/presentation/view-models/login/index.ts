f399ff4ad9c8f91eba7f1dd5c7a37d15c463e37a
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-02-07 19:10:13 -0300

    feat: add template components to LoginView

---
+++ src/presentation/view-models/login/index.ts
@@ -0,0 +1,7 @@
+import { AuthFormHeader } from '../../components/auth-form-header';
+import { KeyboardContainer } from '../../templates/keyboard-container';
+
+export const Login = {
+  KeyboardContainer: KeyboardContainer,
+  Header: AuthFormHeader,
+};
