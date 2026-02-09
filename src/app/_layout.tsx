85c15d406dbb0b151efb72b89aaf7a66d8480b1f
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-02-09 19:53:45 -0300

    feat: restructure RootLayout to include new pages, GestureHandlerRootView and ToastManager

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,16 +1,22 @@
 import { Stack } from 'expo-router';
 import { QueryClientProvider } from '@tanstack/react-query';
-import '../styles/global.css';
-import { client } from '../lib/react-query';
+import '@styles/global.css';
+import { client } from '@lib/react-query';
+import { Modal } from '@presentation/components/modal';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import ToastManager from 'toastify-react-native';
 
 export default function RootLayout() {
   return (
-    <QueryClientProvider client={client}>
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="login" />
-        <Stack.Screen name="register" />
-        <Stack.Screen name="(private)" />
-      </Stack>
-    </QueryClientProvider>
+    <GestureHandlerRootView className="flex-1">
+      <QueryClientProvider client={client}>
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="(public)" />
+          <Stack.Screen name="(private)" />
+        </Stack>
+        <Modal />
+        <ToastManager modal={false} />
+      </QueryClientProvider>
+    </GestureHandlerRootView>
   );
 }
