d58b58102ea5cf0b8213ea4445b548fbb45a1dda
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-02-09 20:09:11 -0300

    feat: add constants for http codes,queryKeys and stale times

--- src/shared/constants/index.ts
+++ src/shared/constants/index.ts
@@ -1,5 +1,16 @@
 export const MIN_NAME_LENGTH = 4;
 export const MIN_PASSWORD_LENGTH = 6;
+
 export const INTERNAL_SERVER_ERROR = 500;
+export const UNAUTHORIZED_ERROR = 401;
+
 export const IOS_API_BASE_URL = 'http://localhost:3001';
 export const ANDROID_API_BASE_URL = 'http://10.0.2.2:3001';
+
+export const PRODUCTS_INFINITY_QUERY_STALE_TIME = 1000 * 60 * 1; // 1 min
+export const PRODUCTS_CATEGORIES_QUERY_STALE_TIME = 1000 * 60 * 60; // 1 h
+
+export const QUERY_KEYS = {
+  PRODUCTS: ['products'],
+  PRODUCTS_CATEGORIES: ['products-categories'],
+};
