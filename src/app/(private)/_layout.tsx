dbf9b1a5a3230e6d32962f740a279a0a64638066
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-02-09 19:59:07 -0300

    feat: enhance PrivateRootLayout with user authentication check and redirect

--- src/app/(private)/_layout.tsx
+++ src/app/(private)/_layout.tsx
@@ -1,5 +1,20 @@
-import { Stack } from 'expo-router';
+import { BottomSheet } from '@presentation/templates';
+import { useUserStore } from '@shared/store/user.store';
+import { Redirect, Stack } from 'expo-router';
 
 export default function PrivateRootLayout() {
-  return <Stack />;
+  const user = useUserStore((state) => state.user);
+  const token = useUserStore((state) => state.token);
+
+  if (!user || !token) {
+    return <Redirect href={'/(public)/login'} />;
+  }
+  return (
+    <>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack>
+      <BottomSheet />
+    </>
+  );
 }
