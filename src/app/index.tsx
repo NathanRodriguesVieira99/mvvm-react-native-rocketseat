7865c4b39be5bb2949bd0a8b6e1842b18e6b02e0
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-02-09 19:58:26 -0300

    feat: implement user authentication redirects based on session state

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,13 @@
+import { useUserStore } from '@shared/store/user.store';
 import { Redirect } from 'expo-router';
 
 export default function App() {
-  return <Redirect href={'/login'} />;
+  const user = useUserStore((state) => state.user);
+  const token = useUserStore((state) => state.token);
+
+  if (user && token) {
+    return <Redirect href={'/(private)/home'} />;
+  }
+
+  return <Redirect href={'/(public)/login'} />;
 }
