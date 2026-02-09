f454b06e895fe477fa110e5a2f75f8d804416d43
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-02-09 20:02:07 -0300

    feat: add Subtitle and Title components for auth form header

--- src/presentation/components/auth-form-header/_components/title.tsx
+++ src/presentation/components/auth-form-header/_components/title.tsx
@@ -2,7 +2,7 @@ import { Text } from 'react-native';
 
 export const Title = ({ title }: { title: string }) => {
   return (
-    <Text className="text-3xl font-bold mb-3 text-center text-gray-500">
+    <Text className="mb-3 text-center text-3xl font-bold text-gray-500">
       {title}
     </Text>
   );
