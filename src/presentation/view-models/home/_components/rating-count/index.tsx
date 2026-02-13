fc8dce7ef46ced9aef377c9513ad0b0cf9f04ef2
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-02-12 21:13:27 -0300

    feat: replace RatingCount component folder

--- src/presentation/view-models/home/_components/rating-count/index.tsx
+++ src/presentation/view-models/home/_components/rating-count/index.tsx
@@ -1,8 +1,8 @@
-import { Ionicons } from '@expo/vector-icons';
-import type { Product } from '@shared/interfaces/product';
-import { colors } from '@styles/colors';
 import type { FC } from 'react';
 import { Text, View } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { colors } from '@styles/colors';
+import type { Product } from '@shared/interfaces/product';
 
 interface RatingCountProps {
   product: Product;
@@ -12,7 +12,9 @@ export const RatingCount: FC<RatingCountProps> = ({ product }) => {
   return (
     <View className="absolute right-0 top-0 flex-row items-center rounded-b-lg rounded-r-none bg-white px-2 py-1">
       <Ionicons name="star" size={12} color={colors['blue-base']} />
-      <Text className="ml-1 text-sm font-semibold">{product.ratingCount}</Text>
+      <Text className="ml-1 text-sm font-semibold">
+        {product.ratingCount.toFixed(1)}
+      </Text>
     </View>
   );
 };
