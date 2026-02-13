0d2e67148139891a495675586a3a1accd8bcfe60
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-02-12 21:17:30 -0300

    feat: enhance useFilterModel with filter handling functions and state management

--- src/presentation/view-models/home/_components/filter/useFilter.model.ts
+++ src/presentation/view-models/home/_components/filter/useFilter.model.ts
@@ -1,5 +1,7 @@
 import type { GetProductsCategoriesService } from '@services/products/get-products-categories.service';
 import { useProductsCategoriesQuery } from '@shared/queries/products/useProductsCategories.query';
+import { useBottomSheetStore } from '@shared/store/bottom-sheet.store';
+import { useFilterStore } from '@shared/store/filter.store';
 
 interface useFilterModelProps {
   getProductsCategoriesService: GetProductsCategoriesService;
@@ -11,5 +13,56 @@ export const useFilterModel = ({
   const { productsCategories, isLoading, error, refetch } =
     useProductsCategoriesQuery(getProductsCategoriesService);
 
-  return { productsCategories, isLoading };
+  const updateFilter = useFilterStore((s) => s.updateFilter);
+  const filterState = useFilterStore((s) => s.filterState);
+  const applyFilters = useFilterStore((s) => s.applyFilters);
+  const resetFilters = useFilterStore((s) => s.resetFilters);
+
+  const close = useBottomSheetStore((s) => s.close);
+
+  const handleValueMaxChange = (value: number) => {
+    updateFilter({ key: 'valueMax', value });
+  };
+
+  const handleValueMinChange = (value: number) => {
+    updateFilter({ key: 'valueMin', value });
+  };
+
+  const handleCategoryToggle = (categoryId: number) => {
+    const categoryAlreadyInArray =
+      filterState.selectedCategories.includes(categoryId);
+
+    if (categoryAlreadyInArray) {
+      updateFilter({
+        key: 'selectedCategories',
+        value: filterState.selectedCategories.filter((id) => id !== categoryId),
+      });
+    } else {
+      updateFilter({
+        key: 'selectedCategories',
+        value: [...filterState.selectedCategories, categoryId],
+      });
+    }
+  };
+
+  const handleApplyFilters = () => {
+    applyFilters();
+    close();
+  };
+
+  const handleResetFilters = () => {
+    resetFilters();
+    close();
+  };
+
+  return {
+    productsCategories,
+    selectedCategories: filterState.selectedCategories,
+    isLoading,
+    handleValueMinChange,
+    handleValueMaxChange,
+    handleCategoryToggle,
+    handleApplyFilters,
+    handleResetFilters,
+  };
 };
