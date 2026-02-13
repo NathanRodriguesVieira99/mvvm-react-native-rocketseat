5f7d50a75e9fc6ef63247d03a4ca99e947d19b92
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-02-12 21:11:45 -0300

    refactor: enhance useProductsInfinityQuery to accept filters and use default perPage value

--- src/shared/queries/products/useProducts.infinity-query.ts
+++ src/shared/queries/products/useProducts.infinity-query.ts
@@ -1,14 +1,22 @@
 import { GetProductsService } from '@services/products/get-products.service';
 import {
+  DEFAULT_PER_PAGE_VALUE,
   PRODUCTS_INFINITY_QUERY_STALE_TIME,
   QUERY_KEYS,
 } from '@shared/constants';
 import { useInfiniteQuery } from '@tanstack/react-query';
 import { BuildImageUrl } from '@helpers/build-image-url';
+import type { FilterStates } from '@shared/store/filter.store';
 
-export const useProductsInfinityQuery = (
-  getProductsService: GetProductsService,
-) => {
+interface ProductsInfinityQueryParams {
+  getProductsService: GetProductsService;
+  filters?: FilterStates;
+}
+
+export const useProductsInfinityQuery = ({
+  getProductsService,
+  filters,
+}: ProductsInfinityQueryParams) => {
   const {
     data,
     error,
@@ -19,14 +27,20 @@ export const useProductsInfinityQuery = (
     refetch,
     isRefetching,
   } = useInfiniteQuery({
-    queryKey: [QUERY_KEYS.PRODUCTS],
+    queryKey: [QUERY_KEYS.PRODUCTS, filters],
     staleTime: PRODUCTS_INFINITY_QUERY_STALE_TIME,
     queryFn: async ({ pageParam = 1 }) => {
       try {
         const response = await getProductsService.exec({
           pagination: {
             page: pageParam,
-            perPage: 10,
+            perPage: DEFAULT_PER_PAGE_VALUE,
+          },
+          filters: {
+            categoryIds: filters?.selectedCategories ?? [],
+            maxValue: filters?.valueMax ?? undefined,
+            minValue: filters?.valueMin ?? undefined,
+            searchText: filters?.searchText ?? undefined,
           },
         });
         return response;
