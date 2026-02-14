4177d51c8bfa1e793d8a854a92a4fde64cd34b22
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-02-14 19:59:38 -0300

    feat: add product details screen to PrivateRootLayout

--- src/app/(private)/_layout.tsx
+++ src/app/(private)/_layout.tsx
@@ -13,6 +13,7 @@ export default function PrivateRootLayout() {
     <>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="product/[id]" options={{ headerShown: false }} />
       </Stack>
       <BottomSheetTemplate />
     </>
