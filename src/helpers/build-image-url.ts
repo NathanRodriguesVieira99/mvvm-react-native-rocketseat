85f1be56926bce8230aa68378c7944bd099ea637
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-02-23 18:12:53 -0300

    fix: handle empty originalUrl in BuildImageUrl function

--- src/helpers/build-image-url.ts
+++ src/helpers/build-image-url.ts
@@ -4,6 +4,8 @@ import { Platform } from 'react-native';
 export const BuildImageUrl = (originalUrl: string): string => {
   if (Boolean(Constants.expoGoConfig?.extra?.isProduction)) return originalUrl;
 
+  if (!originalUrl) return '';
+
   return Platform.select({
     android: originalUrl.replace('localhost', '10.0.2.2'),
     ios: originalUrl,
