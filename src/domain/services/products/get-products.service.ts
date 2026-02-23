15f5638627da01d48f36dbd421898cc7517fc7a7
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-02-23 18:12:40 -0300

    refactor: update GetProductsService to return PaginationResponse<Product> instead of GetProductResponse

--- src/domain/services/products/get-products.service.ts
+++ src/domain/services/products/get-products.service.ts
@@ -1,22 +1,23 @@
-import type { HttpClient } from '@api/http-client';
 import { HttpMethod, type IHttpClient } from '@api/http-client.types';
 import type { GetProductsRequest } from '@shared/interfaces/http/product-request';
-import type { GetProductResponse } from '@shared/interfaces/http/product-response';
+import type { PaginationResponse } from '@shared/interfaces/pagination-response';
+import type { Product } from '@shared/interfaces/product';
 
 interface GetProductsServiceContract {
-  exec: (data: GetProductsRequest) => Promise<GetProductResponse>;
+  exec: (data: GetProductsRequest) => Promise<PaginationResponse<Product>>;
 }
 
 export class GetProductsService implements GetProductsServiceContract {
   constructor(private readonly httpClient: IHttpClient) {}
 
   async exec(body: GetProductsRequest) {
-    const getProductsResponse =
-      await this.httpClient.request<GetProductResponse>({
-        endpoint: '/products',
-        method: HttpMethod.POST,
-        body,
-      });
+    const getProductsResponse = await this.httpClient.request<
+      PaginationResponse<Product>
+    >({
+      endpoint: '/products',
+      method: HttpMethod.POST,
+      body,
+    });
 
     return getProductsResponse;
   }
