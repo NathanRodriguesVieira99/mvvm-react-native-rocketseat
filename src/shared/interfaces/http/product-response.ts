b369c7b3a8ab3a1c104543029019382e45e5c650
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-02-23 18:08:50 -0300

    refactor: remove unused GetProductResponse interface

--- src/shared/interfaces/http/product-response.ts
+++
@@ -1,9 +0,0 @@
-import type { Product } from '../product';
-
-export interface GetProductResponse {
-  data: Product[];
-  page: number;
-  perPage: number;
-  total: number;
-  totalPages: number;
-}
