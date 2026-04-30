50ac27aefcc7b65511cf1a84ca4153a594000d41
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2026-04-30 18:36:47 -0300

    feat(cart): implement AddToCartModal component with product confirmation and navigation options

--- src/presentation/screens/product/_components/add-to-cart-modal/index.tsx
+++ src/presentation/screens/product/_components/add-to-cart-modal/index.tsx
@@ -1,3 +1,47 @@
-export const AddToCartModal = () => {
-  return;
+import { Text, View } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { colors } from '@styles/colors';
+
+import type { FC } from 'react';
+import { Button } from '@presentation/components/button';
+
+interface AddToCartModalProps {
+  productName: string;
+  onGoToCart: () => void;
+  onClose: () => void;
+  onContinueShopping: () => void;
+}
+
+export const AddToCartModal: FC<AddToCartModalProps> = ({
+  productName,
+  onGoToCart,
+  onClose,
+  onContinueShopping,
+}) => {
+  return (
+    <View className="w-full max-w-sm rounded-xl bg-white p-6">
+      <View className="mb-4 items-center">
+        <View className="mb-3 h-16 w-16 items-center justify-center rounded-full bg-green-100">
+          <Ionicons name="checkmark" size={32} color={colors.success} />
+        </View>
+
+        <Text className="text-center text-xl font-bold text-gray-900">
+          Produto adicionado !
+        </Text>
+      </View>
+      <Text className="mb-6 text-center text-gray-600">
+        <Text className="font-semibold">{productName}</Text> Foi adicionado ao
+        seu carrinho com sucesso!
+      </Text>
+
+      <View className="gap-3">
+        <Button onPress={onGoToCart} leftIcon="cart">
+          Ver carrinho
+        </Button>
+        <Button onPress={onContinueShopping} variant="outlined">
+          Continuar comprando
+        </Button>
+      </View>
+    </View>
+  );
 };
